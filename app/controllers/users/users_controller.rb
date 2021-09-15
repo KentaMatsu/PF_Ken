dbe09f1f56ec304b986cdd677fad8f1337e6198f
KentaMatsu <matsuken102@gmail.com>
2021-09-15 09:33:21 +0000

    add_users

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,19 +1,31 @@
 class Users::UsersController < ApplicationController
 
   def index
-    
+    @users = User.all
+    @user = current_user
   end
 
   def show
-    
+    @user = user.find(params[:id])
+    @posts = @user.posts.all
+
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
-  
+
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      #flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def check
