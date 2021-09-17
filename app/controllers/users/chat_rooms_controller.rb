b4f7a0cebdd408871af90d80d0ab83c66cfc0d80
KentaMatsu <matsuken102@gmail.com>
2021-09-17 12:59:11 +0000

    add_chat

--- app/controllers/users/chat_rooms_controller.rb
+++ app/controllers/users/chat_rooms_controller.rb
@@ -1,7 +1,35 @@
 class Users::ChatRoomsController < ApplicationController
+
   def index
+    @chat_room = Chat_room.new
+    @chat_rooms = Chat_room.all
   end
 
   def show
+    @chat_room = Chat_room.find(params[:id])
+    @chat = Chat.new
   end
+
+  def create
+    @chat_room = Chat_room.new(chat_room_params)
+    @chat_room.user_id = current_user.id
+    if @chat_room.save
+      # flash[:notice] = "You have created book successfully."
+      redirect_to chat_room_path(@chat_room)
+    else
+      @chat_rooms = Chat_room.all
+      render :index
+    end
+  end
+
+  def destroy
+    @chat_room = Chat_room.find(params[:id])
+    @chat_room.destroy
+    redirect_to chat_rooms_path
+  end
+
+  def chat_room_params
+    params.require(:chat_room).permit(:room_name)
+  end
+
 end
--- app/controllers/users/chats_controller.rb
+++ app/controllers/users/chats_controller.rb
@@ -1,2 +1,26 @@
 class Users::ChatsController < ApplicationController
+
+  def create
+    @chat_room = Chat_room.find(params[:chat_room_id])
+    chat = Chat.new(chat_params)
+    chat.chat_room_id = @chat_room.id
+    chat.user_id = current_user.id
+    if chat.save
+      redirect_to chat_room_path
+    else
+      render 'chat_rooms/show'
+    end
+  end
+
+  def destroy
+    @chat_room = Chat_room.find(params[:chat_room_id])
+    chat = @chat_room.chat.find(params[:id])
+    chat.destroy
+    redirect_to chat_room_path
+  end
+
+  def chat_params
+    params.require(:chat).permit(:response)
+  end
+
 end
