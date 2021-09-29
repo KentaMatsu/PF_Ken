0e689333da5d3007c1090dcf12289fd8b164b6cd
KentaMatsu <matsuken102@gmail.com>
2021-09-29 20:22:35 +0000

    add_admin_contacts

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -2,4 +2,12 @@ class Admins::ContactsController < ApplicationController
 
   before_action :authenticate_admin!
 
+  def index
+    @contacts = Contact.all.reverse_order
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
 end
